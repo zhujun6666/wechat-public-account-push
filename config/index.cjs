15df343c849ca59e343d8f8681c52d90c01ff82a
zhujun6666 <62698311+zhujun6666@users.noreply.github.com>
2022-10-04 20:12:34 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -22,7 +22,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'opD4U5_yCNHQr7Vy82fpn9GYiWq0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'S2JYfI0hSXUYINg015GDSDWkojGONy4GKGalWcMeVDs',
+      useTemplateId: 'opp51tDRU1Z5-NgW_wlVhx2lxQdOH2H9oUhJ90ZeOso',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '07-02',
       festivals: [
@@ -45,7 +45,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'S2JYfI0hSXUYINg015GDSDWkojGONy4GKGalWcMeVDs',
+  CALLBACK_TEMPLATE_ID: 	opp51tDRU1Z5-NgW_wlVhx2lxQdOH2H9oUhJ90ZeOso',
 
   CALLBACK_USERS: [
     {
