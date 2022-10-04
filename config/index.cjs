648c74537709b2dc4505dae8c4a84f1fa1c16c62
zhujun6666 <62698311+zhujun6666@users.noreply.github.com>
2022-10-04 15:41:20 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '	-nbH0ztdf3-MJCzesgotRsrl3A2FMc3KUuO0h39C5UA',
+  CALLBACK_TEMPLATE_ID: '-nbH0ztdf3-MJCzesgotRsrl3A2FMc3KUuO0h39C5UA',
 
   CALLBACK_USERS: [
     {
